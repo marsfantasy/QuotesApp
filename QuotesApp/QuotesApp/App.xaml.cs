ef9ea8349d95160cd59e3e406f9c5d5729cce93d
梁世慶 Mars Liang <marsfantasy@gmail.com>
2020-06-19 22:03:11 +0800

    加入專案檔案。

---
+++ QuotesApp/QuotesApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FreshMvvm;
+using QuotesApp.PageModels;
+using QuotesApp.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QuotesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new WelcomPage();
+            MainPage = FreshPageModelResolver.ResolvePageModel<WelcomePageModel>();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ QuotesApp/QuotesApp/PageModels/WelcomePageModel.cs
@@ -0,0 +1,39 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reactive.Concurrency;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace QuotesApp.PageModels
+{
+    public class WelcomePageModel : FreshBasePageModel
+    {
+        public Command SignInCommand { get; set; }
+
+        private string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                userName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        public WelcomePageModel()
+        {
+            SignInCommand = new Command(SignIn);
+        }
+
+        private void SignIn()
+        {
+            var user = UserName;
+        }
+    }
+}
